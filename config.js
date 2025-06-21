5aeffbfcbda05c78afea7a61676dacb62e32a2e3
wwwmattiaa <logmattia@gmail.com>
2025-06-21 23:23:31 +0200

    config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.botnumber = "" //Inserisci nelle apici il numero del bot (es. +3937620304
 global.confirmCode = ""
 
 global.owner = [
-    ['393509368693', 'Proprietario', true ],
+    ['393519263592', 'Proprietario', true ],
 ] //Numero degli owner
 
 //────────────────────────────
